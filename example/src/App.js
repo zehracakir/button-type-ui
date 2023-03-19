606465c98fd45fe42b3fe85293855d40d086ecad
zehracakir <cakirzehra312@gmail.com>
2023-03-19 15:53:51 +0300

    running a button component in npmjs

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-import { ExampleComponent } from 'button-type-ui'
+import { Button } from 'button-type-ui'
 import 'button-type-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return(
+  <>
+    <Button text="Text Button" type="text"/>
+    <br /><br />
+    <Button text="Default Button " type="default"/>
+    <br /><br />
+    <Button text="Dashed Button" type="dashed"/>
+    <br /><br />
+    <Button text="Primary Button" type="primary"/>
+    <br /><br />
+    <Button text="Link Button" type = "link"/>
+  </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = ({type,text}) =>{
+  return <button className={`${styles.btn} ${styles[type]}`}>{text}</button>
 }
